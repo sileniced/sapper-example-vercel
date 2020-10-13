c80e84d2625426d0ea0be83c3b7ec8d8225e8ace
Vince Paul Liem <vincepaul.liem@gmail.com>
2020-10-13 23:22:18 +0200

    before firebase

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,48 @@
+<script>
+  import Nav from '../components/Nav.svelte';
+
+  export let segment;
+</script>
+
+<svelte:head>
+  <link href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" rel="stylesheet">
+  <style global>
+      :root {
+          --background-body: #0f0e17;
+          --text-main: #a7a9be;
+          --text-bright: #fffffe;
+          --text-muted: #242629;
+          --links: #ff8906;
+          --focus: #242629;
+          --border: black;
+          --code: #f25f4c;
+          --animation-duration: #242629;
+          --button-hover: #ff8906;
+          --form-placeholder: #f25f4c;
+          --form-text: #fffffe;
+      }
+
+      @media (prefers-color-scheme: light) {
+          :root {
+              --background-body: #eff0f3;
+              --text-main: #2a2a2a;
+              --text-bright: #0d0d0d;
+              --text-muted: #fffffe;
+              --links: #ff8e3c;
+              --focus: #242629;
+              --border: #0d0d0d;
+              --code: #d9376e;
+              --animation-duration: #242629;
+              --button-hover: #ff8e3c;
+              --form-placeholder: #d9376e;
+              --form-text: #0d0d0d;
+          }
+      }
+  </style>
+</svelte:head>
+
+<Nav {segment}/>
+
+<main>
+  <slot></slot>
+</main>
